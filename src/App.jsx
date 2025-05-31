5fe3f58621269ae05a38b6e92cea10275dc67fa2
Enowtabi <enowbibi@gmail.com>
2025-05-31 16:08:32 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex items-center justify-center w-screen h-screen'>
+      <span className='text-amber-500 text-3xl font-bold font-serif'>Code For Change 2025</span>
+    </div>
+  )
+}
+
+export default App
